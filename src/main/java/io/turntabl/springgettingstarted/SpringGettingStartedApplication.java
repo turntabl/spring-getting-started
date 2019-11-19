64b144efdaaa2d43aaa2000b865f2dd292a9d656
agyen <izagyen96@gmail.com>
2019-11-19 14:59:51 +0000

    spring

--- src/main/java/io/turntabl/springgettingstarted/SpringGettingStartedApplication.java
+++ src/main/java/io/turntabl/springgettingstarted/SpringGettingStartedApplication.java
@@ -8,9 +8,13 @@ public class SpringGettingStartedApplication {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
 
-		HumanBean person = (HumanBean)ac.getBean("humanBean");
+		HumanBean person = (HumanBean)ac.getBean("identity");
+		//HumanBean bestie = (HumanBean)ac.getBean("bf");
 
 		System.out.println(person.fname);
+		System.out.println(person.lname);
+		System.out.println(person.bestFriend.fname);
+//		System.out.println(bestie.bestFriend.lastName);
 	}
 
 }
